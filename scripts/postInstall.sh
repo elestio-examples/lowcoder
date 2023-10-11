e9635efe875c15bf6b22032f11076ea69b8ddd75
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-11 12:31:27 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,3 +4,15 @@ set -o allexport; source .env; set +o allexport;
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
 sleep 30s;
+
+target=$(docker-compose port lowcoder-api-service 3000)
+
+curl http://${target}/api/auth/form/login \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36' \
+  --data-raw '{"register":true,"loginId":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","source":"EMAIL","authId":"EMAIL"}' \
+  --compressed
