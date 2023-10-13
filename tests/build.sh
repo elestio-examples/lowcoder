3d21a4b5f00c86ec9e0baba6dbdb16007d1dad66
Prem chavhan <pchavhan@logiquad.com>
2023-10-13 12:40:33 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cp ./deploy/docker/Dockerfile ./
+sed -i "s~npm install -g yarn~npm install -g yarn --network-timeout 300000~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/lowcoder:latest | docker load
